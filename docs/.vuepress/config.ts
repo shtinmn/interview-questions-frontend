f255e6ad02f031689ead80c6b40f420354d55945
Shtin Maksim <shtinmn@gmail.com>
2021-10-27 23:22:02 +0300

    feat: добавлен typescript

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,18 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  base: '/interview-questions-frontend/',
+  lang: 'ru',
+  title: 'Вью(Vue) интерВью',
+  description: 'Данный проект создан для удобного проведения интервью',
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+
+    navbar: [
+      // { text: 'Home', link: '/' },
+      { text: 'Javascript', link: '/javascript/' },
+      // { text: 'External', link: 'https://google.com' }
+    ],
+  },
+})
--- gh-pages.js
+++
@@ -1,5 +0,0 @@
-var ghpages = require('gh-pages')
-
-ghpages.publish('docs/.vuepress/dist', function(err) {
-  console.log('publish err: ', err)
-})
